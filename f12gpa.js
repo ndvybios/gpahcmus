365b29024748c2b0a4074a9d3fdf638de36f1d31
ndvybios <70304367+ndvybios@users.noreply.github.com>
2022-10-23 10:00:04 +0700

    Rename f12gpa.txt to f12gpa.js

---
+++ f12gpa.js
@@ -0,0 +1,19 @@
+﻿var tinchi = document.querySelectorAll("td:nth-child(3)");
+var monhoc = document.querySelectorAll("td:nth-child(2)");
+var diem = document.querySelectorAll("td:nth-child(6)");
+var diemtren = 0,
+diemduoi = 0;
+for (var i = 1; i < tinchi.length; i++) {
+if (
+monhoc[i].innerText.includes("Thể dục") ||
+monhoc[i].innerText.includes("Anh văn") ||
+monhoc[i].innerText.includes("Tin học") ||
+monhoc[i].innerText.includes("Giáo dục") || Number(diem[i].innerText) < 5
+) {
+continue;
+}
+diemtren += Number(tinchi[i].innerText) * Number(diem[i].innerText);
+diemduoi += Number(tinchi[i].innerText);
+}
+console.log("Tong tin chi : " + diemduoi);
+console.log("Diem trung binh : " + diemtren / diemduoi);
